c2af34b88d7124cbd1628bd1ac89bf7e91d39483
SoumyoNawab8 <gtechxd@gmail.com>
2019-08-01 14:45:30 +0530

    updated MERN Branch

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -32,7 +32,7 @@ class Movie extends Component {
     fetch(endpoint)
     .then(result => result.json())
     .then(result => {
-      console.log
+      // console.log
       if (result.status_code) {
         this.setState({ loading: false });
       } else {
