4e93ec31d075cd7f45b96a13775c654f2526f366
SoumyoNawab8 <gtechxd@gmail.com>
2019-08-02 18:49:34 +0530

    updated gitignore

---
+++ build/precache-manifest.400a30e938d559ffb6ce04e859a0b310.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a8919b7f79eed558595d5e4acf789f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4269c22482b04edadd5d",
+    "url": "/static/css/main.3d809292.chunk.css"
+  },
+  {
+    "revision": "517d23890691cc37933b",
+    "url": "/static/js/2.b521379e.chunk.js"
+  },
+  {
+    "revision": "4269c22482b04edadd5d",
+    "url": "/static/js/main.59cd349f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
